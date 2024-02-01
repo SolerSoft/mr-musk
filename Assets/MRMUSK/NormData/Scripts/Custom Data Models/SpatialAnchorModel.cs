4b705cb2afb99cd24b2f0307cf460b1052c8fce5
Jared Bienz <jbienz@solersoft.com>
2024-02-01 15:36:08 -0600

    Moved merged files to match folder structure.

---
+++ Assets/MRMUSK/NormData/Scripts/Custom Data Models/SpatialAnchorModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RealtimeModel]
+public partial class SpatialAnchorModel
+{
+    [RealtimeProperty(1,true,true)]
+    private string _spatialAnchorID;
+}
