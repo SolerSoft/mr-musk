b9e89f1c9971f52ea019d7fd68028751c446f3bb
Jared Bienz <jared.bienz@global.komatsu>
2024-02-15 00:54:03 -0600

    Working on alignment still

--- Assets/MRMUSK/Colocation/ColocationManager.cs
+++ Assets/MRMUSK/Colocation/ColocationManager.cs
@@ -44,7 +44,7 @@ namespace SolerSoft.MRMUSK.Colocation
                 if (!_isColocating)
                 {
                     // We are now co-locating
-                    _isColocating = true;
+                    //_isColocating = true;
 
                     // Colocate to world reference
                     Colocate();
@@ -98,23 +98,35 @@ namespace SolerSoft.MRMUSK.Colocation
             ColocateTo(_worldCenterReference);
         }
 
+        /*
         /// <summary>
         /// Colocates to the specified position and rotation.
         /// </summary>
-        /// <param name="worldCenter">
+        /// <param name="worldPosition">
         /// The world center to colocate to.
         /// </param>
         /// <param name="worldRotation">
         /// The world rotation to colocate to.
         /// </param>
-        public void ColocateTo(Vector3 worldCenter, Quaternion worldRotation)
+        public void ColocateTo(Vector3 worldPosition, Quaternion worldRotation)
         {
-            // Move player rig opposite of position, which will make the position now 0,0,0
-            _playerRig.transform.Translate(-worldCenter);
+            // Move player rig opposite of position, which will make the position now 0,0,0 _playerRig.transform.Translate(-worldCenter);
 
             // Rotate the player rig to the opposite Y angle of rotation
-            _playerRig.transform.Rotate(Vector3.up, worldRotation.eulerAngles.y * -1);
+            // _playerRig.transform.Rotate(Vector3.up, worldRotation.eulerAngles.y * -1);
+            // _playerRig.transform.localEulerAngles = new Vector3(0, -worldRotation.eulerAngles.y, 0);
+
+            // Calculate the relative position of GOB from the perspective of GOA
+            Vector3 relativePosition = _playerRig.InverseTransformPoint(worldPosition);
+
+            // Calculate the relative rotation of GOB from the perspective of GOA
+            Quaternion relativeRotation = Quaternion.Inverse(_playerRig.rotation) * worldRotation;
+
+            // Move GOA to make GOB appear at position (0,0,0) and rotation (0,0,0) from its perspective
+            _playerRig.position -= relativePosition;
+            // _playerRig.rotation *= Quaternion.Euler(0, -relativeRotation.eulerAngles.y, 0);
         }
+        */
 
         /// <summary>
         /// Colocates to the specified transform.
@@ -124,7 +136,40 @@ namespace SolerSoft.MRMUSK.Colocation
         /// </param>
         public void ColocateTo(Transform transform)
         {
-            ColocateTo(transform.position, transform.rotation);
+            // Get difference between player rig to transform
+            Vector3 rigToTransformDistance = _playerRig.position - transform.position;
+
+            // Get difference between player rig to transform rotation
+            float rigToTransformAngle = _playerRig.rotation.eulerAngles.y - transform.rotation.eulerAngles.y;
+
+            /*
+            // Translate
+            _playerRig.Translate(-transform.position);
+
+            // Rotate first
+            _playerRig.Rotate(Vector3.up, -transform.rotation.eulerAngles.y, Space.World);
+            // _playerRig.rotation = Quaternion.Euler(0, -transform.rotation.eulerAngles.y, 0);
+
+            // _playerRig.Translate(transform.InverseTransformPoint(Vector3.zero));
+            */
+
+            // Calculate the translation and rotation needed to reset the child's world position and rotation
+            Vector3 childPosition = transform.position;
+            Quaternion childRotation = transform.rotation;
+
+            // Reverse the child's position and rotation to get the offset needed for the parent
+            Vector3 offsetPosition = -childPosition;
+            Quaternion offsetRotation = Quaternion.Inverse(childRotation);
+
+            // Apply the offset to the parent's position and rotation _playerRig.rotation =
+            // Quaternion.Euler(0, offsetRotation.eulerAngles.y - rigToTransformAngle, 0);
+            // _playerRig.Rotate(Vector3.up, -transform.rotation.eulerAngles.y +
+            // rigToTransformAngle, Space.World);
+            _playerRig.Rotate(Vector3.up, -transform.rotation.eulerAngles.y, Space.World);
+
+            // _playerRig.position += offsetPosition; _playerRig.Translate(-rigToTransformDistance);
+            // _playerRig.position = -(transform.position - rigToTransformDistance);
+            _playerRig.Translate(-transform.position);
         }
 
         #endregion Public Methods
